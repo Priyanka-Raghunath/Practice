7a0f640bc9b40b3db130fa78159a6831c7ea7ec3
Priyanka-Raghunath <101509150+Priyanka-Raghunath@users.noreply.github.com>
2023-10-20 18:23:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+function handle(button){    
+    switch (button){
+        case "w":
+            var audio=new Audio("./sounds/tom-1.mp3");
+            audio.play();
+        break;
+        case "a":
+            var audio=new Audio("./sounds/tom-2.mp3");
+            audio.play();
+        break;
+        case "s":
+            var audio=new Audio("./sounds/tom-3.mp3");
+            audio.play();
+        break;
+        case "d":
+            var audio=new Audio("./sounds/tom-4.mp3");
+            audio.play();
+        break;
+        case "j":
+            var audio=new Audio("./sounds/crash.mp3");
+            audio.play();
+        break;
+        case "k":
+            var audio=new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+        break;
+        case "l":
+            var audio=new Audio("./sounds/snare.mp3");
+            audio.play();
+        break;
+        default:
+            pass
+            break;
+    }
+}
+function buttonAnimation(currentKey){
+    document.querySelector("."+currentKey).classList.add("pressed");
+    setTimeout(function(event){
+        document.querySelector("."+currentKey).classList.remove("pressed"); // removes the pressed style
+    }, 100); // takes two parameters: function and time
+}
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        button=this.textContent;
+        handle(button);
+        buttonAnimation(button)
+    });
+}
+
+document.addEventListener("keydown",function(event){
+    handle(event.key);
+    buttonAnimation(event.key);
+});
